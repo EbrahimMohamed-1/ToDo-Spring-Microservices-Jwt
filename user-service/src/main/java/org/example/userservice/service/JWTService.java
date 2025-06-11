0fa492c8285639db85d392bb884c4e44e695ddf0
Ibrahim mohamed <EbrahimMohammed10101@gmail.com>
2025-06-11 17:53:52 +0300

    Update JWT service

--- user-service/src/main/java/org/example/userservice/service/JWTService.java
+++ user-service/src/main/java/org/example/userservice/service/JWTService.java
@@ -1,7 +1,17 @@
 package org.example.userservice.service;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.userservice.entity.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Service
 public class JWTService {
 
     @Value("${env_variables.JWT_SECRET}")
@@ -10,6 +20,65 @@ public class JWTService {
     @Value("${env_variables.JWT_EXPIRATION_MS}")
     public static String JWT_EXPIRATION_MS;
 
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    }
+
+    public String createToken(User user, Map<String, Object> extraClaims) {
+        extraClaims.put("userId", user.getId());
+        extraClaims.put("roles", List.of("USER"));
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_MS))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    public Claims resolveClaims(HttpServletRequest req) {
+        try {
+            String token = resolveToken(req);
+            if (token != null) {
+                return parseJwtClaims(token);
+            }
+            return null;
+        } catch (ExpiredJwtException ex) {
+            req.setAttribute("expired", ex.getMessage());
+            throw ex;
+        } catch (JwtException ex) {
+            req.setAttribute("invalid", ex.getMessage());
+            throw ex;
+        }
+    }
+
+    private Claims parseJwtClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isTokenExpired(Date expirationDate) {
+        return expirationDate.before(new Date());
+    }
+
+    public List<String> getRoles(Claims claims) {
+        return claims.get("roles", List.class);
+    }
 
 
 }
+
+
+
