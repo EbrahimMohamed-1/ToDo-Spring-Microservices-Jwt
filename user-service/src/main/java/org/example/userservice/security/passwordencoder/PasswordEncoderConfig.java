caeec90bb9df0e39320946c0c9d1be1d4d2b77ee
Ibrahim mohamed <EbrahimMohammed10101@gmail.com>
2025-06-11 19:37:48 +0300

    Update password encoder

--- user-service/src/main/java/org/example/userservice/security/passwordencoder/PasswordEncoderConfig.java
+++ user-service/src/main/java/org/example/userservice/security/passwordencoder/PasswordEncoderConfig.java
@@ -1,6 +1,8 @@
 package org.example.userservice.security.passwordencoder;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -8,7 +10,7 @@ public class PasswordEncoderConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
 
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
 
     }
 }
